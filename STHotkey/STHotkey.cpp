55d45a525e62de518504310f4dcb4f91e46c67fd
ProSureString <prosurestring@prosurestring.xyz>
2025-10-25 14:17:49 -0700

    use std

--- STHotkey/STHotkey.cpp
+++ STHotkey/STHotkey.cpp
@@ -1,22 +1,24 @@
 #include <windows.h>
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
 	if (!RegisterHotKey(NULL, 1, MOD_CONTROL | MOD_ALT, 0x53)) // S is 0x53 i thnk
 	{
-		std::cerr << "Failed to register hotkey. error code: " << GetLastError() << std::endl;
+		cerr << "Failed to register hotkey. error code: " << GetLastError() << endl;
 		return 1;
 	}
 
-	std::cout << "Hotkey reg success, ctrl alt s to trigger. listening,.. " << std::endl;
+	cout << "Hotkey reg success, ctrl alt s to trigger. listening,.. " << endl;
 
 	MSG msg = { 0 };
 	while (GetMessage(&msg, NULL, 0, 0) != 0)
 	{
 		if (msg.message == WM_HOTKEY)
 		{
-			std::cout << "triggerded hotkey1 !!" << std::endl;
+			cout << "triggerded hotkey1 !!" << endl;
 		}
 	}
 
