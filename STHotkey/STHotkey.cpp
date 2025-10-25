56bd2d6fca13ffa3be2bb604d54d4bfb923d36c8
ProSureString <prosurestring@prosurestring.xyz>
2025-10-25 16:20:20 -0700

    hooker

--- STHotkey/STHotkey.cpp
+++ STHotkey/STHotkey.cpp
@@ -4,25 +4,35 @@
 
 HHOOK g_hHook = NULL;
 
+LRESULT CALLBACK LLKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode >= 0) {
+		KBDLLHOOKSTRUCT* kb = (KBDLLHOOKSTRUCT*)lParam;
+		if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
+			std::cout << "VK: " << kb->vkCode << " sc: " << kb->scanCode << std::endl;
+		}
+	}
+	return CallNextHookEx(g_hHook, nCode, wParam, lParam);
+}
+
 int main()
 {
-	if (!RegisterHotKey(NULL, 1, MOD_CONTROL | MOD_ALT, 0x53)) // S is 0x53 i thnk
+	g_hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LLKeyboardProc, GetModuleHandle(NULL), 0);
+	if (!g_hHook)
 	{
-		std::cerr << "Failed to register hotkey. error code: " << GetLastError() << std::endl;
+		std::cerr << "Failed to become a hooker. error code: " << GetLastError() << std::endl;
 		return 1;
 	}
 
-	std::cout << "Hotkey reg success, ctrl alt s to trigger. listening,.. " << std::endl;
+	std::cout << "hook installed, press stuff to see vk codes,.. " << std::endl;
 
-	MSG msg = { 0 };
-	while (GetMessage(&msg, NULL, 0, 0) != 0)
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
 	{
-		if (msg.message == WM_HOTKEY)
-		{
-			std::cout << "triggerded hotkey1 !!" << std::endl;
-		}
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
 	}
 
-	UnregisterHotKey(NULL, 1);
+	UnhookWindowsHookEx(g_hHook);
 	return 0;
 }
